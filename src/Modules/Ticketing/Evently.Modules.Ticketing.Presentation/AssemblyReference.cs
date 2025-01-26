077ea90b0c8c33481ec9a76a838708fddd559bb4
Onur Güzel <ongguzel@gmail.com>
2025-01-26 17:57:52 +0300

    Scaffolding the Ticketing Module

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Presentation/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+namespace Evently.Modules.Ticketing.Presentation;
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
